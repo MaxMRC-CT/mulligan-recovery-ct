0bdafe8153bace4fdcb29167611eba9c1a9622a2
MaxMRC-CT <max@mulliganrecoveryct.com>
2026-02-16 14:08:31 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </a>
         <SiteHeader />
         <main id="main-content" className="relative overflow-hidden bg-[#f5f4f1]">
-          <div aria-hidden="true" className="pointer-events-none absolute -right-52 top-[560px] h-[1160px] w-[1160px] opacity-[0.06]">
+          <div aria-hidden="true" className="pointer-events-none absolute -right-52 top-[560px] z-20 h-[1160px] w-[1160px] opacity-[0.06]">
             <Image src="/logo-circle.png" alt="" fill sizes="920px" className="object-contain" />
           </div>
-          <div className="relative z-10">{children}</div>
+          <div className="relative z-0">{children}</div>
         </main>
         <SiteFooter />
       </body>
